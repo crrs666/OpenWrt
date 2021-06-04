7ebfb25e03abcf3ca51c2f53142fe7122c4d4324
crrs666 <fdjwd@foxmail.com>
2021-06-05 02:04:48 +0800

    use original kernel

--- devices/redmi-ac2100/diy.sh
+++ devices/redmi-ac2100/diy.sh
@@ -2,7 +2,4 @@ rm -Rf target/linux/{ramips,generic}
 svn co https://github.com/crrs666/lede/trunk/target/linux/ramips target/linux/ramips
 svn co https://github.com/crrs666/lede/trunk/target/linux/generic target/linux/generic
 
-rm -rf include/kernel-version.mk
-wget -O include/kernel-version.mk https://raw.githubusercontent.com/crrs666/lede/master/include/kernel-version.mk
-
 sed -i 's?admin/status/channel_analysis??' package/feeds/luci/luci-mod-status/root/usr/share/luci/menu.d/luci-mod-status.json
