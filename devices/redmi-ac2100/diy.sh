8536a6c47f1bfc347f21836f2bf3c9f97e1166d6
crrs666 <fdjwd@foxmail.com>
2021-06-05 12:08:47 +0800

    remove usage of community openwrt

--- devices/redmi-ac2100/diy.sh
+++ devices/redmi-ac2100/diy.sh
@@ -1,5 +1,1 @@
-rm -Rf target/linux/{ramips,generic}
-svn co https://github.com/crrs666/lede/trunk/target/linux/ramips target/linux/ramips
-svn co https://github.com/crrs666/lede/trunk/target/linux/generic target/linux/generic
-
 sed -i 's?admin/status/channel_analysis??' package/feeds/luci/luci-mod-status/root/usr/share/luci/menu.d/luci-mod-status.json
